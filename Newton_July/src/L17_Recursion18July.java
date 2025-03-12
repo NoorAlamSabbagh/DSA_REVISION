7361b040de7ec5d421b89ba52dc7455e73734d9f
NoorAlamSabbagh <alamrangrej787@gmail.com>
2025-03-13 03:29:31 +0530

    revise two pointers

--- Newton_July/src/L17_Recursion18July.java
+++ Newton_July/src/L17_Recursion18July.java
@@ -59,7 +59,8 @@ public class L17_Recursion18July {
 
 //Code 4
 
-/*public class L17_Recursion18July {
+/*
+public class L17_Recursion18July {
 
     public static void printDigitsWithRecursion(int n) {
         if(n==0)
@@ -92,45 +93,45 @@ public class L17_Recursion18July {
 
 //Code 5
 
-public class L17_Recursion18July {
-
-    public static void printDigitsWithRecursion(int n) {
-        if(n==0)
-            return;
-
-        int r = n%10;
-        //System.out.println(r);
-        printDigitsWithRecursion( n/10);
-        System.out.print(r);
-    }
-
-    public static int binarySearch(int arr[],int n, int x){
-
-        int l = 0;
-        int r = n-1;
-
-        while (l<=r) {
-            int m = l + (r-l)/2;
-
-            if(arr[m] == x){
-                return m;
-            }
-            else if(arr[m]<x){
-                l = m+1;
-            }
-            else{
-                r = m-1;
-            }
-        }
-        // Value not found
-        return -1;
-    }
-    public static void main(String[] args) {
-
-        int arr[] = {10,25,30,42,100};
-        int n = arr.length;
-        int x = 30;
-        System.out.println(binarySearch(arr,n,x));
-    }
-
-}
+//public class L17_Recursion18July {
+//
+//    public static void printDigitsWithRecursion(int n) {
+//        if(n==0)
+//            return;
+//
+//        int r = n%10;
+//        //System.out.println(r);
+//        printDigitsWithRecursion( n/10);
+//        System.out.print(r);
+//    }
+//
+//    public static int binarySearch(int arr[],int n, int x){
+//
+//        int l = 0;
+//        int r = n-1;
+//
+//        while (l<=r) {
+//            int m = l + (r-l)/2;
+//
+//            if(arr[m] == x){
+//                return m;
+//            }
+//            else if(arr[m]<x){
+//                l = m+1;
+//            }
+//            else{
+//                r = m-1;
+//            }
+//        }
+//        // Value not found
+//        return -1;
+//    }
+//    public static void main(String[] args) {
+//
+//        int arr[] = {10,25,30,42,100};
+//        int n = arr.length;
+//        int x = 30;
+//        System.out.println(binarySearch(arr,n,x));
+//    }
+//
+//}
